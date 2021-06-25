de7b5e90cd599127eecb3a1cb61922db414c1af1
Joris Langlois <joris.langlois@knplabs.com>
2021-06-25 16:57:34 +0300

    wip burger menu

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,31 +1,46 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import "./menu.scss"
 
 // Menu :: Props -> React.Component
-export default () =>
-  <header>
-    <p className="logo-amo">
-      <Link to="/">AMO</Link>
-    </p>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/concerts">Concerts</Link>
-          <div className="separator"></div>
-        </li>
-        <li>
-          <Link to="/ecouter">Écouter</Link>
-          <div className="separator"></div>
-        </li>
-        <li>
-          <Link to="/pro">Pro</Link>
-          <div className="separator"></div>
-        </li>
-        <li>
-          <Link to="/boutique">Boutique</Link>
-          <div className="separator"></div>
-        </li>
-      </ul>
-    </nav>
-  </header>
+export default () => {
+  const [opened, setOpened] = useState(false);
+
+  return (
+    <header>
+      <p className="logo-amo">
+        <Link to="/">AMO</Link>
+      </p>
+
+      <div
+        className="menu-burger"
+        onClick={() => opened ? setOpened(false) : setOpened(true)}
+      >
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+
+      <nav className={opened ? "opened" : "closed"}>
+        <ul>
+          <li>
+            <Link to="/concerts">Concerts</Link>
+            <div className="separator"></div>
+          </li>
+          <li>
+            <Link to="/ecouter">Écouter</Link>
+            <div className="separator"></div>
+          </li>
+          <li>
+            <Link to="/pro">Pro</Link>
+            <div className="separator"></div>
+          </li>
+          <li>
+            <Link to="/boutique">Boutique</Link>
+            <div className="separator"></div>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
