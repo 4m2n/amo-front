a43eacd3e3099850962f6d81ad20708974f7fb12
Joris Langlois <joris.langlois@knplabs.com>
2021-06-28 20:09:06 +0300

    desktop version of the landing page

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -2,6 +2,7 @@ import React from "react"
 import Contact from "./../contact/contact"
 import Biography from "./../biography/biography"
 import bkg from "./../../assets/images/AMO_bkg.jpg"
+import meandres from "./../../assets/images/meandres.png"
 import "./homepage.scss"
 
 // Homepage :: () -> React.Component
@@ -16,7 +17,27 @@ const Homepage = () =>
         />
       </figcaption>
     </figure>
+
     <Biography />
+
+    <div className="extended-play">
+      <div className="container">
+        <figure>
+          <img src={meandres} alt="méandres" />
+        </figure>
+        <aside>
+          <h1>Nouvel EP</h1>
+          <h2>« MÉANDRES »</h2>
+          <p>
+            5 titres<br/>
+            23'
+          </p>
+          <p>
+            Disponible en CD
+          </p>
+        </aside>
+      </div>
+    </div>
   </section>
 
 export default Homepage
