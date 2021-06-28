06aef4dd2fe5bb18c1638074d9527d6f1a154ad3
Joris Langlois <joris.langlois@knplabs.com>
2021-06-28 19:26:32 +0300

    working bio section

--- src/components/biography/biography.js
+++ src/components/biography/biography.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import parser from "./../../parser/parser"
+import parse from "./../../parser/parser"
+import "./biography.scss"
 
 // Biography :: () -> React.Component
 export default function Biography() {
+  const [bioIsVisible, setBioIsVisible] = useState(false)
+
   const data = useStaticQuery(graphql`
     query HeaderQuery {
       markdownRemark(frontmatter: {id: {eq: "biography"}}) {
@@ -17,9 +20,19 @@ export default function Biography() {
   `)
 
   return (
-    <section className="biography">
+    <section className="biography container">
       <h1>{data.markdownRemark.frontmatter.title}</h1>
-      {parser(data.markdownRemark.htmlAst)}
+
+      <article className={bioIsVisible ? "is-expanded" : "is-collapsed"}>
+        {parse(data.markdownRemark.htmlAst)}
+      </article>
+
+      <p
+        className="expand-bio"
+        onClick={() => bioIsVisible ? setBioIsVisible(false) : setBioIsVisible(true)}
+      >
+        > {bioIsVisible ? "Refermer" : "Lire la bio complète"}
+      </p>
     </section>
   )
 }
