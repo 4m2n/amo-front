efb39bfb5352b612ed2899745633f8d4012a75f1
Joris Langlois <joris.langlois@knplabs.com>
2021-08-13 13:56:18 +0300

    add the brochure to the pro section

--- src/components/pro/pro.js
+++ src/components/pro/pro.js
@@ -1,19 +1,30 @@
 import React from "react"
 import technicalFile from "./../../assets/pdf/AMO_fiche_technique.pdf"
+import marketingFile from "./../../assets/pdf/AMO_brochure.pdf"
 import "./pro.scss"
 
 // Pro :: () -> React.Component
 const Pro = () =>
   <section className="pro container">
     <h1 className="title">Espace pro</h1>
-    <a
-      title="Télécharger la fiche technique"
-      href={technicalFile}
-      target="_blank"
-    >
-      <span className="im">p</span>
-      Fiche technique
-    </a>
+    <div className="documents">
+      <a
+        title="Télécharger la fiche technique"
+        href={technicalFile}
+        target="_blank"
+      >
+        <span className="im">p</span>
+        Fiche technique
+      </a>
+      <a
+        title="Télécharger la brochure"
+        href={marketingFile}
+        target="_blank"
+      >
+        <span className="im">p</span>
+        Brochure
+      </a>
+    </div>
   </section>
 
 export default Pro
