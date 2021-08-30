21a78f8d97100aea6999bfe4a2d8f9b9731ac104
Joris Langlois <joris.langlois@knplabs.com>
2021-08-30 14:50:45 +0300

    replaced assets with new ones

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -6,22 +6,20 @@ import Image from "./../image"
 import LatestInterview from "./latest-interview"
 import NextShow from "./../tour/next-show"
 import "./homepage.scss"
-import banner from "./../../assets/images/banner-1.jpg"
 
 // Homepage :: () -> React.Component
 const Homepage = () =>
   <section className="homepage">
     <figure className="banner">
-      {/* <Image
+      <Image
         id="homepage-banner"
         filename="AMO_bkg"
         alt="mout"
         sizes={[768, 1024, 1216, 1408]}
         formats={["webp", "jpg"]}
         width={768}
-        height={257}
-      /> */}
-      <img src={banner} />
+        height={155}
+      />
       <figcaption>
         <Contact showSocialNetworks={true} />
       </figcaption>
