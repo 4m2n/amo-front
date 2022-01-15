f2d0f1fa5fa21e7843cce4eccd1ea670bdaf61fa
malmoot <85483999+malmoot@users.noreply.github.com>
2022-01-15 13:09:20 +0100

    maj lien youtube
    maj lien youtube : nouvelle vidéo

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -31,7 +31,7 @@ const Homepage = () =>
     <section className="latest-clip">
       <div className="container">
         <YoutubeVideo
-          src="https://www.youtube.com/embed/us9VMkiWV1k"
+          src="https://www.youtube.com/watch?v=dBkR7Jr-zCE"
         />
       </div>
     </section>
